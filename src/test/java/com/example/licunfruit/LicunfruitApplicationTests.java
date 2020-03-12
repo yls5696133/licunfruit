e3f6fd63d58985fe4244f968fc5dd154b9459b0c
YAO LinSheng <number@lednets.com>
2020-03-12 17:01:14 +0800

    黎村水果店第一次提交

---
+++ src/test/java/com/example/licunfruit/LicunfruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.licunfruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicunfruitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
